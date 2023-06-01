0ae67787c6d323bf85e66be7a8f8a2932027b62d
Arno Sonck <arno.sonck@r-n0.com>
2023-05-31 22:42:46 -0600

    El botón de seleccionar mustra que ChronoBest fue seleccionada.

--- js/chronobeast.js
+++ js/chronobeast.js
@@ -1,1 +1,30 @@
-alert("Holis")
+function startGame(){
+    let buttonChronoBeastPlayer = document.getElementById('button-slectChronoBeast')
+    buttonChronoBeastPlayer.addEventListener('click',playerSelection)
+
+
+    
+}
+
+function playerSelection(){
+    let topoTejonPlayer = document.getElementById('topotejon')
+    let carneroKanguro = document.getElementById('carnerokanguro')
+    let osoLeón = document.getElementById('osoleon')
+    let spamPlayerChronoBeast = document.getElementById('playerChronoBeast')
+
+    if(topoTejonPlayer.checked){
+        spamPlayerChronoBeast.innerHTML = "TopoTejon"
+    } else if (carneroKanguro.checked){
+        spamPlayerChronoBeast.innerHTML = "CarneroKanguro"
+    } else if (osoLeón.checked){
+        spamPlayerChronoBeast.innerHTML = "OsoLeón"
+    } else{
+        alert('Select a ChronoBeast')
+    }
+}
+
+function selectChronoBeastPlayer(){
+    alert("Holis")
+}
+
+window.addEventListener('load',startGame)
